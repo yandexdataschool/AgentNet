1dd9f7880c4ed60655c6bc7e4a6000c4c983d250
jheuristic <justheuristic@gmail.com>
2016-10-27 01:44:45 +0300

    deprecation

--- agentnet/resolver/__init__.py
+++ agentnet/resolver/__init__.py
@@ -1,9 +1,12 @@
 """
-Layers that convert Q-value vectors into action ids
+Layers that convert policy or Q-value vectors into action ids
 """
 
 from .base import *
 from .epsilon_greedy import *
 from .probabilistic import *
 
-from ..deprecated import deprecated
+
+def ProbabilisticResolver(*args,**kwargs):
+    raise ValueError("Use Probabilistic resolver (with i)")
+
